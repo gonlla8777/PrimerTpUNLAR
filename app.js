cbfa5f919e1ee063fecb017b77a8247d26bcca4d
Gonzalo <utngonzalollanos@gmail.com>
2022-09-04 22:36:04 -0300

    Primer ejercicio listo

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const countries = require('./countries.json');
  */
 function getCountryByIso3(isoCode){
     //Complete with your code
+    const buscador = countries.find(country => country.iso3 == isoCode)
+    console.log(buscador)
 }
 
 /**
